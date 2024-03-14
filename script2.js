c6044ef8054fb8d9e5705528044fd1d859c79afd
Dekaron <dalibordvorak1311gmail.com>
2024-03-14 17:34:45 +0100

    Mobile page2

--- script2.js
+++ script2.js
@@ -2,16 +2,28 @@ const questionElement = document.getElementById('question')//link to button
 let questionIndex=0;
 let econ=localStorage.getItem('Economy');
 let social=localStorage.getItem('Social');
-let grid=localStorage.getItem('Size');
 
 
 function checkLoadedPage() {
     var currentURL = window.location.href;
     if (currentURL.includes("quizcompass")) {
+        function gcd(a, b) {
+            while (b !== 0) {
+                let temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }  
+        function lcm(a, b) {
+            return 2*((a * b) / gcd(a, b));
+        }
+        grid=lcm(socialquestions.length,economyquestions.length);
+
        Nextq();
     } 
     else if (currentURL.includes("cres")) {
-        grid=20
+        grid=questions.length;
         let table = document.getElementById("MyTable");
         for (let i = 0; i < grid/2; i++) {
                 let row = document.createElement('tr');
@@ -51,21 +63,21 @@ function checkLoadedPage() {
       ////////////////////////
       //POSITION ON COMPASS///
       ///////////////////////
-      let x=econ;
-      let y=social;
+      let x=grid-(grid/2-econ);
+      let y=grid-(grid/2-social);
       var whatRow = table.querySelector("tr:nth-child("+x+")");
       var whatCell = whatRow.querySelector("td:nth-child("+y+")");
-      whatCell.innerHTML = "X";
+      whatCell.style.backgroundColor = "black"
     }
 }
 window.onload = checkLoadedPage;
 
 
-function ultbtn(x,lol){
-    if(question.answears=="Social"){
+function ultbtn(x){
+    if(questions[questionIndex].answears=="Social"){
         social+=x
     }
-    else if(question.answears=="Economy"){
+    else if(questions[questionIndex].answears=="Economy"){
         econ+=x
     }
     questionIndex += 1;  
@@ -77,15 +89,30 @@ function ultbtn(x,lol){
 function Nextq(){
     if (questionIndex < questions.length) {
         questionElement.innerText = questions[questionIndex].question; // update text
+        
     } else {
     // Storing the data:
         localStorage.setItem('Social',social);
         localStorage.setItem('Economy',econ);
-        localStorage.setItem('Size',questions.length);
         window.location.replace('cres.html');
             }}
 
-const questions=[
+const socialquestions=[
+    {
+        question:'Abortions',
+        answears:'Social',
+    },
+    {
+        question:'Democracy',
+        answears:'Social',
+    },
+    {
+        question:'LGBT+ rights',
+        answears:'Social',
+    }
+];
+
+const economyquestions=[
     {
         question:'Free Health care',
         answears:'Economy',
@@ -95,7 +122,43 @@ const questions=[
         answears:'Economy',
     },
     {
-        question:'LGBT+ rights',
-        answears:'Social',
+        question:'Gold',
+        answears:'Economy',
     },
-]
+    {
+        question:'Free Health care',
+        answears:'Economy',
+    },
+    {
+        question:'Free food',
+        answears:'Economy',
+    },
+    {
+        question:'Free Health care',
+        answears:'Economy',
+    },
+    {
+        question:'Free food',
+        answears:'Economy',
+    },
+    {
+        question:'Free Health care',
+        answears:'Economy',
+    },
+    {
+        question:'Free food',
+        answears:'Economy',
+    }
+];
+
+function mixArrays(array1, array2) {
+    let combinedArray = array1.concat(array2);
+    for (let i = combinedArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [combinedArray[i], combinedArray[j]] = [combinedArray[j], combinedArray[i]];
+    }
+    
+    return combinedArray;
+}
+
+const questions = mixArrays(socialquestions, economyquestions);
