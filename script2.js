575784ff002aa02eb558e82c5d6f4b2412a06af0
Dekaron <dalibordvorak1311gmail.com>
2024-04-15 17:54:59 +0200

    Pozice na kompase

--- script2.js
+++ script2.js
@@ -1,73 +1,22 @@
-const questionElement = document.getElementById('question')//link to button
+const questionElement = document.getElementById('question')
 let questionIndex=0;
-let econ=localStorage.getItem('Economy');
-let social=localStorage.getItem('Social');
+let econ=parseInt(localStorage.getItem('Economy'))|| 0;;
+let social=parseInt(localStorage.getItem('Social'))|| 0;;
 
 
 function checkLoadedPage() {
     var currentURL = window.location.href;
     if (currentURL.includes("quizcompass")) {
-        function gcd(a, b) {
-            while (b !== 0) {
-                let temp = b;
-                b = a % b;
-                a = temp;
-            }
-            return a;
-        }  
-        function lcm(a, b) {
-            return 2*((a * b) / gcd(a, b));
-        }
-        grid=lcm(socialquestions.length,economyquestions.length);
-
        Nextq();
     } 
     else if (currentURL.includes("cres")) {
-        grid=questions.length;
-        let table = document.getElementById("MyTable");
-        for (let i = 0; i < grid/2; i++) {
-                let row = document.createElement('tr');
-                table.appendChild(row);
-                // Create cells
-                for (let i = 0; i < grid/2; i++) {
-                    var newCell = document.createElement('td');
-                    newCell.classList.add("kl");
-                    newCell.innerHTML = "";
-                    row.appendChild(newCell);
-
-              }
-              for (let i = grid/2; i < grid; i++) {
-                var newCell = document.createElement('td');
-                newCell.classList.add("kp");
-                newCell.innerHTML = "";
-                row.appendChild(newCell);
-          }
-          }
-          for (let i = grid/2; i < grid; i++) {
-            let row = document.createElement('tr');
-            table.appendChild(row);
-            // Create cells
-            for (let i = 0; i < grid/2; i++) {
-                var newCell = document.createElement('td');
-                newCell.classList.add("ll");
-                newCell.innerHTML = "";
-                row.appendChild(newCell);
-          }
-          for (let i = grid/2; i < grid; i++) {
-            var newCell = document.createElement('td');
-            newCell.classList.add("lp");
-            newCell.innerHTML = "";
-            row.appendChild(newCell);
-      }
-      }
-      ////////////////////////
-      //POSITION ON COMPASS///
-      ///////////////////////
-      let x=grid-(grid/2-econ);
-      let y=grid-(grid/2-social);
-      var whatRow = table.querySelector("tr:nth-child("+x+")");
-      var whatCell = whatRow.querySelector("td:nth-child("+y+")");
-      whatCell.style.backgroundColor = "black"
+        const circle = document.getElementById("circle");
+        let v1= (document.getElementById("box2").offsetWidth/2)+(((document.getElementById("MyTable").offsetWidth-circle.offsetWidth) / 40) * econ);
+        let v2= (document.getElementById("box2").offsetHeight/2)+(((document.getElementById("MyTable").offsetHeight-circle.offsetHeight) / 40) * social);
+        console.log(econ, social,document.getElementById("MyTable").offsetWidth);
+        circle.style.left=v1;
+        circle.style.top=v2;
+      
     }
 }
 window.onload = checkLoadedPage;
@@ -81,23 +30,38 @@ function ultbtn(x){
         econ+=x
     }
     questionIndex += 1;  
-    console.log(questionIndex);
+    console.log(econ, social);
     Nextq()
 }
 
 
 function Nextq(){
     if (questionIndex < questions.length) {
-        questionElement.innerText = questions[questionIndex].question; // update text
+        questionElement.innerText = questions[questionIndex].question;
         
     } else {
-    // Storing the data:
         localStorage.setItem('Social',social);
         localStorage.setItem('Economy',econ);
         window.location.replace('cres.html');
             }}
 
 const socialquestions=[
+    {
+        question:'Democracy',
+        answears:'Social',
+    },
+    {
+        question:'LGBT+ rights',
+        answears:'Social',
+    },
+    {
+        question:'LGBT+ rights',
+        answears:'Social',
+    },
+    {
+        question:'LGBT+ rights',
+        answears:'Social',
+    },
     {
         question:'Abortions',
         answears:'Social',
@@ -107,7 +71,19 @@ const socialquestions=[
         answears:'Social',
     },
     {
-        question:'LGBT+ rights',
+        question:'Abortions',
+        answears:'Social',
+    },
+    {
+        question:'Democracy',
+        answears:'Social',
+    },
+    {
+        question:'Abortions',
+        answears:'Social',
+    },
+    {
+        question:'Democracy',
         answears:'Social',
     }
 ];
@@ -143,6 +119,10 @@ const economyquestions=[
     },
     {
         question:'Free Health care',
+        answears:'Economy',
+    },
+    {
+        question:'Free food',
         answears:'Economy',
     },
     {
