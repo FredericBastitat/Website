e0da768b106fbcdcb7bafc333708c68b508801ce
Dekaron <dalibordvorak1311gmail.com>
2024-04-23 16:58:57 +0200

    text added troley

--- script1.js
+++ script1.js
@@ -63,17 +63,17 @@ const questions=[
         ]
     },
     {
-        question:'B)There is a runaway trolley barreling down the railway tracks. Ahead, on the tracks, there are five people tied up and unable to move. The trolley is headed straight for them. You are standing some distance off in the train yard, next to a lever. If you pull this lever, the trolley will switch to a different set of tracks. However, you notice that there is one person on the side track. You have two (and only two) options:',
+        question:'B)A train full of passengers is going under a bridge on which you and a very fat man are standing. The tracks are broken though and if you do nothing the people inside the train will die in the crash. You have two (and only two) options:',
         answears:[
-            {text: 'B)Do nothing, in which case the trolley will kill the five people on the main track.'},
-            {text: 'B)Pull the lever, diverting the trolley onto the side track where it will kill one person.'}
+            {text: 'B)Do nothing, in which case all passangers will die.'},
+            {text: 'B)Knock the fat man down to stop the train and save all the passangers (the fat man dies).'}
         ]
     },
     {
-        question:'C)There is a runaway trolley barreling down the railway tracks. Ahead, on the tracks, there are five people tied up and unable to move. The trolley is headed straight for them. You are standing some distance off in the train yard, next to a lever. If you pull this lever, the trolley will switch to a different set of tracks. However, you notice that there is one person on the side track. You have two (and only two) options:',
+        question:'C)There are 10 sick patients waiting for a transplant and one healthy patient who is under anaesthesia waiting for an appendix operation. There are no donors and 10 patients die. You have two (and only two) options:',
         answears:[
-            {text: 'C)Do nothing, in which case the trolley will kill the five people on the main track.'},
-            {text: 'C)Pull the lever, diverting the trolley onto the side track where it will kill one person.'}
+            {text: 'C)Do nothing and let 10 patients die.'},
+            {text: 'C)cut up a healthy patient into organs and save the 10 who need a transplant.'}
         ]
     }
 ]
